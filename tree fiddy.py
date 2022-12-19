461bec88709a38ce94e8c6e4bd959f14d0c8fa66
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-12-19 10:59:26 +0100

    commit

--- tree fiddy.py
+++ tree fiddy.py
@@ -1,7 +1,14 @@
 def LagoNess():
     frase="Siempre va a tratarse de Tree Fiddy"
-    search=frase.find("Tree Fiddy")
-    print(search)
+    if frase.find("Tree Fiddy")!=1: #Si encuentra la palabra Tree Fiddy, y el resultado es distinto de 1, entonces que lo sustituya por 3.50
+        search="Tree Fiddy"
+        replace="3.50"
+        print (frase.replace(search,replace))
+    if frase.find("Tree Fiddy")!=-1:
+        search="Tree Fiddy"
+        replace="three fifty"
+        print (frase.replace(search,replace))
+
 
 if __name__=='__main__':
     LagoNess()
