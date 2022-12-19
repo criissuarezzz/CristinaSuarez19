57cd8bb274fb9578793b52dbb298285dbcd38188
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-12-19 09:04:54 +0100

    commits

--- xbonacci.py
+++ xbonacci.py
@@ -4,10 +4,15 @@ import random
 n=int(input("¿Cuántos números quieres como firma de la secuencia?: "))
 firma=[]
 for i in range(n):
-    firma.append(random.rrandint(0,1))
+    firma.append(random.randint(0,1))
 def xbonacci(firma, n):
+    if n==0:
+        return []
     for i in range(n):
         firma.append(sum(firma[-n:]))
     return firma
+
+if __name__=='__main__':
+    print(xbonacci(firma,n))
         
     
