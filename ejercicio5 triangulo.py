a2e447c1ef07384a1d872c689379ca8818f1edf7
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-12-19 10:06:11 +0100

    commit

--- ejercicio5 triangulo.py
+++ ejercicio5 triangulo.py
@@ -1,10 +1,16 @@
+h=int(input("¿De qué altura quieres el triángulo?: "))
+
 def hollow_triangle(h):
-    for i in range(h-i):
+    for i in range(h):
         for j in range(h-i):
             print(" ", end=" ")
         
-        for j in range (2*i+1)
+        for j in range (2*i+1):
+            if j==0 or j==2*i or i==h-1:
+                print("#", end="")
+            else:
+                print("", end="")
+        print()
 
 if __name__=='__main__':
-    h=int(input("¿De qué altura quieres el triángulo?: "))
     hollow_triangle(h)
