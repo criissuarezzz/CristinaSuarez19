89bbb42d6e9257b8a314b49f5249fc3ce8c0422f
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-12-19 11:07:13 +0100

    commit

--- ejercicio 5/ejercicio5 triangulo.py
+++ ejercicio 5/ejercicio5 triangulo.py
@@ -6,12 +6,12 @@ def hollow_triangle(h):
             print("", end="")  #Para generear espacios en blanco
         
         for j in range (2*i+1):
-            if j==0 or j==2*i or i==h-1:
+            if j==0 or j==2*i or i==h-1:   #Para que genere los bordes del triángulo
                 print("#", end="")
-            elif j==h:
-                print("","#", end="")
+            elif j==h:   #Para que cuando h coincida con la última línea la complete con #
+                print("","#", end="")  
             else:
-                print(" ", end="")
+                print(" ", end="")   #Y si no ocurre nada de eso, espacio en blanco
         print()
 
 if __name__=='__main__':
