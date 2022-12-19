8ebf983df99ae6f02ae79c7a1bd1c2e450144369
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-12-19 11:03:06 +0100

    commit

--- xbonacci.py
+++ xbonacci.py
@@ -4,14 +4,14 @@ import random, unittest
 n=int(input("¿Cuántos números quieres como firma de la secuencia?: "))
 firma=[]
 for i in range(n):
-    firma.append(random.randint(0,1))
-def xbonacci(firma, n):
+    firma.append(random.randint(0,1))  #Para que incluya tantos 0 y 1 a la lista firma como su rango(n) indica
+def xbonacci(firma, n):   #Creamos la función xbonacci
     if n==0:
         return []
-    for i in range(n):
-        firma.append(sum(firma[-n:]))
+    for i in range(n):    #Para que la secuencia tenga tantos números como su rango(n) indica
+        firma.append(sum(firma[-n:]))   #Añadimos a la lista firma la suma de los últimos n números
     return firma
-class testxbonacci(unittest.TestCase):
+class testxbonacci(unittest.TestCase):      #test
     def testxbonacci(self):
         self.assertEqual(xbonacci(firma,n))
 
